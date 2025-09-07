0c2f1fd286d77b32a5ca42776393c1040b00b7b9
Mr.Lin <lsx0044@gmail.com>
2025-09-08 01:33:23 +0800

    Update start-chmod.sh

--- start-chmod.sh
+++ start-chmod.sh
@@ -8,7 +8,7 @@ BACKUP_DIR="/mnt/html_backup"
 if [ ! "$(ls -A $TARGET_DIR)" ]; then
   echo "$TARGET_DIR is empty. Copying backup files..."
   # 如果挂载目录为空，则复制备份文件到目标目录
-  cp -r $BACKUP_DIR/* $TARGET_DIR/
+  cp -a $BACKUP_DIR/* $TARGET_DIR/
   echo "Backup files copied to $TARGET_DIR."
 else
   echo "$TARGET_DIR is not empty. No action needed."
